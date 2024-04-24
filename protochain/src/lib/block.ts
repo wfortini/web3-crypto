ed213c0090b5f83903556c32226e6598fb485ce1
wellington.pereira <wellington.pereira@transfero.com>
2024-04-24 20:03:42 -0300

    added crypto-js

--- protochain/src/lib/block.ts
+++ protochain/src/lib/block.ts
@@ -1,5 +1,21 @@
 
-
+import sha256 from 'crypto-js/sha256';
 export default class Block {
-    
+    index: number;
+    timestamp: number;
+    hash: string;
+    previousHash: string;
+    data: string;
+
+    constructor(index: number, hash: string, previousHash: string, data: string){
+        this.index = index;
+        this.hash = this.getHash();
+        this.timestamp = Date.now();
+        this.previousHash = previousHash;
+        this.data = data;
+    }
+
+    getHash(): string {
+         return sha256(this.index + this.data + this.timestamp + this.previousHash).toString();
+    }
 }
